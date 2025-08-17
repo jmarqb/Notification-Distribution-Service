f99869e3b18f64b7cbd52f2e769e0280028430f5
jmarqb <jacmelbalu7@gmail.com>
2025-08-16 22:00:10 -0400

    config(envs):update environment validations

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -8,6 +8,13 @@ interface EnvVars {
   REDIS_PORT: number;
   REDIS_HOST: string;
   JWT_SECRET: string;
+  MAILER_PROVIDER: string;
+  MAILER_GMAIL_EMAIL: string;
+  MAILER_GMAIL_SECRET_KEY: string;
+  APP_EMAIL_MAILGUN_FROM: string;
+  APP_EMAIL_MAILGUN_KEY: string;
+  APP_EMAIL_MAILGUN_DOMAIN: string;
+  APP_EMAIL_MAILGUN_USERNAME: string;
 }
 
 const envsSchema = joi
@@ -17,6 +24,13 @@ const envsSchema = joi
     REDIS_PORT: joi.number().required(),
     REDIS_HOST: joi.string().required(),
     JWT_SECRET: joi.string().required(),
+    MAILER_PROVIDER: joi.string().required(),
+    MAILER_GMAIL_EMAIL: joi.string().required(),
+    MAILER_GMAIL_SECRET_KEY: joi.string().required(),
+    APP_EMAIL_MAILGUN_FROM: joi.string().required(),
+    APP_EMAIL_MAILGUN_KEY: joi.string().required(),
+    APP_EMAIL_MAILGUN_DOMAIN: joi.string().required(),
+    APP_EMAIL_MAILGUN_USERNAME: joi.string().required(),
   })
   .unknown(true);
 
@@ -26,6 +40,13 @@ const { error, value } = envsSchema.validate({
   REDIS_PORT: process.env.REDIS_PORT,
   REDIS_HOST: process.env.REDIS_HOST,
   JWT_SECRET: process.env.JWT_SECRET,
+  MAILER_PROVIDER: process.env.MAILER_PROVIDER,
+  MAILER_GMAIL_EMAIL: process.env.MAILER_GMAIL_EMAIL,
+  MAILER_GMAIL_SECRET_KEY: process.env.MAILER_GMAIL_SECRET_KEY,
+  APP_EMAIL_MAILGUN_FROM: process.env.APP_EMAIL_MAILGUN_FROM,
+  APP_EMAIL_MAILGUN_KEY: process.env.APP_EMAIL_MAILGUN_KEY,
+  APP_EMAIL_MAILGUN_DOMAIN: process.env.APP_EMAIL_MAILGUN_DOMAIN,
+  APP_EMAIL_MAILGUN_USERNAME: process.env.APP_EMAIL_MAILGUN_USERNAME,
 });
 
 if (error) {
@@ -40,4 +61,11 @@ export const envs = {
   redis_port: envVars.REDIS_PORT,
   redis_host: envVars.REDIS_HOST,
   jwt_secret: envVars.JWT_SECRET,
+  mailer_provider: envVars.MAILER_PROVIDER,
+  mailer_gmail_email: envVars.MAILER_GMAIL_EMAIL,
+  mailer_gmail_secret_key: envVars.MAILER_GMAIL_SECRET_KEY,
+  app_email_mailgun_from: envVars.APP_EMAIL_MAILGUN_FROM,
+  app_email_mailgun_key: envVars.APP_EMAIL_MAILGUN_KEY,
+  app_email_mailgun_domain: envVars.APP_EMAIL_MAILGUN_DOMAIN,
+  app_email_mailgun_username: envVars.APP_EMAIL_MAILGUN_USERNAME,
 };
